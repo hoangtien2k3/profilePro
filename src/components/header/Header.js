639aca17ce7dfebdef5b6937e1cc26675356eb5d
hoangtien2k3 <hoangtien2k3qx1@gmail.com>
2024-09-26 13:52:28 +0700

    web-profile-sampl

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -76,9 +76,9 @@ function Header() {
               <a href="#resume">Resume</a>
             </li>
           )}
-          <li>
-            <a href="#contact">Contact Me</a>
-          </li>
+          {/*<li>*/}
+          {/*  <a href="#contact">Contact Me</a>*/}
+          {/*</li>*/}
           <li>
             {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
             <a>
@@ -90,4 +90,5 @@ function Header() {
     </Headroom>
   );
 }
+
 export default Header;
--- src/portfolio.js
+++ src/portfolio.js
@@ -18,10 +18,10 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Hoàng Anh Tiến",
+  title: "Hi all, I'm Tiến",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A passionate Backend Developer and System Design 🚀 with experience building Backend and Mobile applications with Java / Kotlin / Golang / Spring Boot and some other cool libraries and frameworks."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
@@ -30,44 +30,44 @@ const greeting = {
 
 // Social Media Links
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  github: "https://github.com/hoangtien2k3",
+  linkedin: "https://www.linkedin.com/in/hoangtien2k3/",
+  gmail: "hoangtien2k3qx1@gmail.com",
+  gitlab: "https://gitlab.com/hoangtien2k3",
+  facebook: "https://www.facebook.com/hoangtien2k3.vn",
+  medium: "https://medium.com/@hoangtien2k3",
+  stackoverflow: "https://stackoverflow.com/users/17357931/hoangtien2k3?tab=profile",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
 };
 
 // Skills Section
-
 const skillsSection = {
   title: "What I do",
-  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  subTitle: "CRAZY BACKEND DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
   skills: [
     emoji(
-      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+      "⚡ Develop highly interactive Backend / System for your web and mobile applications"
     ),
-    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
     emoji(
-      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+      "⚡ Passionate about Java backend development Reactive programming (reactor-core)"
+    ),
+    emoji(
+      "⚡ Integration of third party services such as Firebase/ AWS / Cloudinary/ Digital Ocean"
     )
   ],
 
   /* Make Sure to include correct Font Awesome Classname to view your icon
 https://fontawesome.com/icons?d=gallery */
-
   softwareSkills: [
     {
-      skillName: "html-5",
-      fontAwesomeClassname: "fab fa-html5"
+      skillName: "Java",
+      fontAwesomeClassname: "fab fa-java"
     },
     {
-      skillName: "css3",
-      fontAwesomeClassname: "fab fa-css3-alt"
+      skillName: "Golang",
+      fontAwesomeClassname: "fab fa-golang"
     },
     {
       skillName: "sass",
@@ -86,8 +86,8 @@ https://fontawesome.com/icons?d=gallery */
       fontAwesomeClassname: "fab fa-node"
     },
     {
-      skillName: "swift",
-      fontAwesomeClassname: "fab fa-swift"
+      skillName: "android",
+      fontAwesomeClassname: "fab fa-android"
     },
     {
       skillName: "npm",
@@ -122,8 +122,8 @@ const educationInfo = {
   display: true, // Set false to hide this section, defaults to true
   schools: [
     {
-      schoolName: "Harvard University",
-      logo: require("./assets/images/harvardLogo.png"),
+      schoolName: "University Of Transport Technology",
+      logo: require("./assets/images/transpotLogo.png"),
       subHeader: "Master of Science in Computer Science",
       duration: "September 2017 - April 2019",
       desc: "Participated in the research of XXX and published 3 papers.",
@@ -168,10 +168,10 @@ const workExperiences = {
   display: true, //Set it to true to show workExperiences Section
   experience: [
     {
-      role: "Software Engineer",
-      company: "Facebook",
-      companylogo: require("./assets/images/facebookLogo.png"),
-      date: "June 2018 – Present",
+      role: "Backend Developer",
+      company: "Viettel Telecom",
+      companylogo: require("./assets/images/viettelTelecomLogo.png"),
+      date: "May 2024 – Present",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       descBullets: [
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
@@ -179,17 +179,17 @@ const workExperiences = {
       ]
     },
     {
-      role: "Front-End Developer",
-      company: "Quora",
-      companylogo: require("./assets/images/quoraLogo.png"),
-      date: "May 2017 – May 2018",
+      role: "Backend Developer Intern",
+      company: "TLH Viet Nam",
+      companylogo: require("./assets/images/tlhVietNamLogo.png"),
+      date: "Feb 2024 – May 2024",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
-      role: "Software Engineer Intern",
-      company: "Airbnb",
-      companylogo: require("./assets/images/airbnbLogo.png"),
-      date: "Jan 2015 – Sep 2015",
+      role: "Backend Developer Intern",
+      company: "Cen Group",
+      companylogo: require("./assets/images/cenGroupLogo.png"),
+      date: "Sep 2023 – Feb 2024",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     }
   ]
@@ -209,25 +209,37 @@ const bigProjects = {
   subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
   projects: [
     {
-      image: require("./assets/images/saayaHealthLogo.webp"),
-      projectName: "Saayahealth",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      image: require("./assets/images/smeViettelLogo.png"),
+      projectName: "Viettel SME",
+      projectDesc: "Mobile service, providing all packages to mobile network",
       footerLink: [
         {
           name: "Visit Website",
-          url: "http://saayahealth.com/"
+          url: "https://sme.viettel.vn/"
         }
         //  you can add extra buttons here.
       ]
     },
     {
-      image: require("./assets/images/nextuLogo.webp"),
-      projectName: "Nextu",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      image: require("./assets/images/viettelLogo.png"),
+      projectName: "Viettel Telecom",
+      projectDesc: "Mobile service, providing all packages to mobile network",
       footerLink: [
         {
           name: "Visit Website",
-          url: "http://nextu.se/"
+          url: "https://vietteltelecom.vn/vx/"
+        }
+        //  you can add extra buttons here.
+      ]
+    },
+    {
+      image: require("./assets/images/fasteeLogo.png"),
+      projectName: "Fastee App",
+      projectDesc: "Fastee online golf course booking application.",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://fastee.vn/"
         }
       ]
     }
@@ -320,7 +332,6 @@ const blogSection = {
 };
 
 // Talks Sections
-
 const talkSection = {
   title: "TALKS",
   subtitle: emoji(
@@ -363,13 +374,13 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+  number: "(+84) 828007853",
+  email_address: "hoangtien2k3qx1@gmail.com"
 };
 
 // Twitter Section
 const twitterDetails = {
-  userName: "twitter", //Replace "twitter" with your twitter username without @
+  userName: "@hoangtien_2k3", //Replace "twitter" with your twitter username without @
   display: true // Set true to display this section, defaults to false
 };
 
